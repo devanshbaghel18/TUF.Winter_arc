35d511fecaa58dbaab4f3bfe4743a2578804a576
devanshbaghel18 <devanshbaghel059@gmail.com>
2025-10-15 20:49:29 +0530

    Starting the other part of pattern problems , i think they are of good level

--- tufwinterarc.cpp
+++ tufwinterarc.cpp
@@ -3,52 +3,70 @@
 using namespace std;
 
 int main(){
- for(int i = 1; i <= 5; i++) {
-        for(int j = 1; j <= 5; j++) {  
-            cout << "*";
-        }
-        cout << endl;  
-    }
+//  for(int i = 1; i <= 5; i++) {
+//         for(int j = 1; j <= 5; j++) {  
+//             cout << "*";
+//         }
+//         cout << endl;  
+//     }
 
-cout<<endl;
+// cout<<endl;
 
-    for(int i = 1; i <= 5; i++) {      
-        for(int j = 1; j <= i; j++) {     
-            cout << "*";
-        }
-        cout << endl;                     
-    }
-cout<<endl;
-    for(int i = 1; i <= 5; i++) {        
-        for(int j = 1; j <= i; j++) {        
-            cout << j;
-        }
-        cout << endl;                         
-    }
-cout<<endl;
+//     for(int i = 1; i <= 5; i++) {      
+//         for(int j = 1; j <= i; j++) {     
+//             cout << "*";
+//         }
+//         cout << endl;                     
+//     }
+// cout<<endl;
+//     for(int i = 1; i <= 5; i++) {        
+//         for(int j = 1; j <= i; j++) {        
+//             cout << j;
+//         }
+//         cout << endl;                         
+//     }
+// cout<<endl;
 
-    for(int i = 1; i <= 5; i++) {        
-        for(int j = 1; j <= i; j++) {      
-            cout << i;
-        }
-        cout << endl;                       
-    }
-    cout<<endl;
+//     for(int i = 1; i <= 5; i++) {        
+//         for(int j = 1; j <= i; j++) {      
+//             cout << i;
+//         }
+//         cout << endl;                       
+//     }
+//     cout<<endl;
 
-    for(int i=50;i<6;i++){
-        for(int j=1;j<=i;j++){
-            cout<<i;
-        }
-        cout<<endl;
-    }
-    cout<<endl;
+//     for(int i=50;i<6;i++){
+//         for(int j=1;j<=i;j++){
+//             cout<<i;
+//         }
+//         cout<<endl;
+//     }
+//     cout<<endl;
 
     
-     for(int i=5;i>=0;i--){
-        for(int j=1;j<=i;j++){
-            cout<<j;
+//      for(int i=5;i>=0;i--){
+//         for(int j=1;j<=i;j++){
+//             cout<<j;
+//         }
+//         cout<<endl;
+//      } 
+    int a=4;
+    int d=1;
+     for(int i=0;i<5;i++){
+        
+        for(int j=0;j<a;j++){
+            cout<<" ";
         }
+        for(int j=0;j<d;j++){
+            cout<<"*";
+        }
+        for(int j=0;j<a;j++){
+            cout<<" ";
+        }
+        a--;
+        d=d+2;
         cout<<endl;
+         
      }
 }
 
